7454a9519bb715fb739e569b089f4ebce05d91f1
Ajithsoftsuave <ajithkumar.palani@softsuave.com>
2024-10-01 16:02:25 +0530

    Fix minor bugs

--- index.js
+++ index.js
@@ -1,32 +1,32 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import userRoutes from './routes/routes.js';
-import sequelize from './db/db.js';
-import cors from 'cors';
+import express from "express";
+import bodyParser from "body-parser";
+import userRoutes from "./routes/routes.js";
+import sequelize from "./db/db.js";
+import cors from "cors";
 const app = express();
-const port = process.argv[2] || 3000;
+const port = process.argv[2] || 5000;
 
 const corsOptions = {
-    origin: '*', // Allow all origins
-    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed methods
-    allowedHeaders: '*' // Allowed headers
+  origin: "*", // Allow all origins
+  methods: ["GET", "POST", "PUT", "DELETE"], // Allowed methods
+  allowedHeaders: "*", // Allowed headers
 };
 app.use(cors(corsOptions));
 // Middleware
 app.use(bodyParser.json());
 
 // Routes
-app.use('/api', userRoutes);
+app.use("/api", userRoutes);
 
-
-sequelize.sync()
-    .then(() => {
-        console.log('Database & tables created!');
-    })
-    .catch(err => {
-        console.error('Unable to create tables:', err);
-    });
+sequelize
+  .sync()
+  .then(() => {
+    console.log("Database & tables created!");
+  })
+  .catch((err) => {
+    console.error("Unable to create tables:", err);
+  });
 // Start server
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
+  console.log(`Server running on port ${port}`);
 });
--- service/shopify/shopify.service.js
+++ service/shopify/shopify.service.js
@@ -1,6 +1,9 @@
 import { ApiVersion } from "@shopify/shopify-api";
 import { createStorefrontApiClient } from "@shopify/storefront-api-client";
-import { getCartDataByID, sendOrderInvoice } from "../../service/shopify/query/cart.js";
+import {
+  getCartDataByID,
+  sendOrderInvoice,
+} from "../../service/shopify/query/cart.js";
 import Shopify from "shopify-api-node";
 
 export const storefrontClientInit = async (storeDetail) => {
@@ -17,16 +20,16 @@ export const storefrontClientInit = async (storeDetail) => {
 };
 
 export const shopifyClientInit = async (storeDetail) => {
-    try {
-        const shopifyClient = await new Shopify({
-            shopName: storeDetail.shop_id,
-            accessToken: storeDetail.shopify_access_token
-        });
-        
-        return shopifyClient;
-    } catch (error) {
-        throw error;
-    }
+  try {
+    const shopifyClient = await new Shopify({
+      shopName: storeDetail.shop_id,
+      accessToken: storeDetail.shopify_access_token,
+    });
+
+    return shopifyClient;
+  } catch (error) {
+    throw error;
+  }
 };
 
 export const fetchCart = async (client, cartID) => {
@@ -46,49 +49,51 @@ export const fetchCart = async (client, cartID) => {
 };
 
 export const createOrder = async (client, param) => {
-    try {
-        const order = await client.order.create({
-          sendReceipt:true,
-          customer: param.customerDetails,
-          line_items: param.lineItems,
-          billing_address: param.billingAddress,
-          shipping_address: param.shippingAddress,
-          current_total_price: param.totalAmount,
-            current_subtotal_price: param.subtotalAmount,
-            current_total_tax: param.totalTaxAmount,
-            subtotal_price: param.subtotalAmount,
-          transactions: [
-            {
-              kind: param.paymentDetail.kind,
-              status: param.paymentDetail.status,
-              amount: param.paymentDetail.amount,
-            },
-          ],
-        });
+  try {
+    const order = await client.order.create({
+      sendReceipt: true,
+      customer: param.customerDetails,
+      line_items: param.lineItems,
+      billing_address: param.billingAddress,
+      shipping_address: param.shippingAddress,
+      current_total_price: param.totalAmount,
+      current_subtotal_price: param.subtotalAmount,
+      current_total_tax: param.totalTaxAmount,
+      subtotal_price: param.subtotalAmount,
+      transactions: [
+        {
+          kind: param.paymentDetail.kind,
+          status: param.paymentDetail.status,
+          amount: param.paymentDetail.amount,
+        },
+      ],
+    });
 
-        if (!order) {
-            throw new Error('Order creation failed');
-        }
-        if (param.paymentDetail.status === 'pending'){
-          const invoiceParams = 
-          {
-            orderId: order.admin_graphql_api_id,
-            email: {
-              to: order.email,
-              from: "udhayakumar.devendran@softsuave.com",
-              subject: "Your Invoice from Your Shop Name",
-              customMessage: "Thank you for your order! Please find your invoice attached."
-            }
-          }
-  
-        const invoiceResponse = await client.graphql(sendOrderInvoice(), invoiceParams);
-        console.log('Invoice sent: ', invoiceResponse);
-        } 
-
-      return order;
-    } catch (error) {
-        console.log(error);
-        throw error;
+    if (!order) {
+      throw new Error("Order creation failed");
     }
-};
+    if (param.paymentDetail.status === "pending") {
+      const invoiceParams = {
+        orderId: order.admin_graphql_api_id,
+        email: {
+          to: order.email,
+          from: "udhayakumar.devendran@softsuave.com",
+          subject: "Your Invoice from Your Shop Name",
+          customMessage:
+            "Thank you for your order! Please find your invoice attached.",
+        },
+      };
 
+      const invoiceResponse = await client.graphql(
+        sendOrderInvoice(),
+        invoiceParams
+      );
+      console.log("Invoice sent: ", invoiceResponse);
+    }
+
+    return order;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
